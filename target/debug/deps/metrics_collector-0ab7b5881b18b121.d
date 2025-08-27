e126e0c5aaf117986a5707cad7c07bf381f91226
Ikenna Nwagbara <ikenna.nwagbara@gmail.com>
2025-08-27 10:27:40 -0400

    Add incremental compilation artifacts for metrics collector
    - Created new binary files for query cache and work products, indicating the use of Rust version 1.88.0.
    - Added a lock file for the incremental compilation process.

---
+++ target/debug/deps/metrics_collector-0ab7b5881b18b121.d
@@ -0,0 +1,5 @@
+C:\Users\ikenn\Projects\TradingPlatform\LoggingEngine\target\debug\deps\metrics_collector-0ab7b5881b18b121.d: metrics-collector\src\main.rs
+
+C:\Users\ikenn\Projects\TradingPlatform\LoggingEngine\target\debug\deps\libmetrics_collector-0ab7b5881b18b121.rmeta: metrics-collector\src\main.rs
+
+metrics-collector\src\main.rs:
